c17882f258b14bf8230d91b91dcae9a79e0bd331
jeremy <jeremydj3@gmail.com>
2023-12-09 19:14:41 +0100

    es1 2022 finito

--- 17-01-2023/es1.cpp
+++ 17-01-2023/es1.cpp
@@ -10,13 +10,66 @@ struct Node{
 
 typedef Node* PNode;
 
-int blackHeight(PNode u){
 
-    return 0;
+//int aux(PNode u){
+//    if(u==nullptr){
+//        return 0;
+//    }
+//
+//    else{
+//        int rissx,risdx;
+//        rissx=aux(u->left);
+//        risdx=aux(u->right);
+//        if((rissx==-1 || risdx!=-1)&& u->right!=nullptr && u->left!=nullptr && rissx!=risdx) {
+//            return -1;
+//        }
+//
+//        int ris;
+//        if(u->left == nullptr){
+//            ris=risdx;
+//        }
+//        else{
+//            ris=rissx;
+//        }
+//
+//        if(u->key=='B');
+//        return ris;
+//
+//
+//    }
+//}
+/**
+ *
+ *
+ * */
+int blackHeight(PNode u){
+    if(u==nullptr){
+        return 0;
+    }
+
+    else{
+        int rissx,risdx;
+        rissx=blackHeight(u->left);
+        risdx=blackHeight(u->right);
+        if(rissx==-1 || risdx==-1 || u->right!=nullptr && u->left!=nullptr && rissx!=risdx) {
+            return -1;
+        }
+
+        int ris;
+        if(u->left == nullptr){
+            ris=risdx;
+        }
+        else{
+            ris=rissx;
+        }
+
+        if(u->col=='B')ris++;
+        return ris;
+    }
 }
 
 int main(){
-    PNode root = new Node{1, new Node{2, nullptr, nullptr, 'R'}, new Node{3, nullptr, nullptr, 'B'}, 'B'};
+    PNode root = new Node{1,  new Node{3, nullptr, new Node{4, nullptr, nullptr, 'B'}, 'B'},nullptr, 'B'};
     cout<<blackHeight(root);
 
     return 0;
