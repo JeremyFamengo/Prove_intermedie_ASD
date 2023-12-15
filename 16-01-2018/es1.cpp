4a966f75e2cbbf905d4d55d3249176935f796170
JeremyFamengo <jeremydj3@gmail.com>
2023-12-15 15:30:19 +0100

    completato es1 2018

---
+++ 16-01-2018/es1.cpp
@@ -0,0 +1,35 @@
+#include "iostream"
+using namespace std;
+
+struct Node{
+    int key;
+    Node* left;
+    Node* right;
+};
+
+typedef Node* PNode;
+
+void simmetrica_aux(PNode u, bool& res){
+    if(!u)return;
+
+    if((u->left && !u->right) || (!u->left && u->right) || (u->left && u->right && u->left->key!=u->right->key)){
+        res=false;
+    }
+
+    simmetrica_aux(u->left,res);
+    simmetrica_aux(u->right,res);
+}
+
+bool simmetrica(PNode u){
+    bool res=true;
+    simmetrica_aux(u,res);
+    return res;
+}
+
+int main(){
+    PNode root = new Node{1, new Node{2, nullptr, nullptr}, new Node{3, nullptr, nullptr}};
+
+    cout<<"k_limitato? "<< simmetrica(root);
+
+    return 0;
+}
