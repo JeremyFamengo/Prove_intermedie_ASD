2ad33e8551ce6a3b572979544ad4f4593290bc73
jeremy <jeremydj3@gmail.com>
2024-01-06 10:37:14 +0100

    es1R 2020 finito di nuovo

---
+++ 13-01-2020/es1R.cpp
@@ -0,0 +1,52 @@
+#include "iostream"
+using namespace std;
+
+struct Node{
+    int key;
+    Node* left;
+    Node* right;
+};
+
+typedef Node* PNode;
+
+struct T{
+    PNode root;
+};
+
+typedef T* Tree;
+
+void antenatoAux(PNode u, int k1, bool& tk1, int k2, bool& tk2, bool& res){
+    if(!u)return;
+
+    if(k1==u->key)tk1=true;
+    if(k2==u->key)tk2=true;
+
+    if(!tk1 && tk2){
+        res=false;
+        return;
+    }
+
+    antenatoAux(u->left,k1,tk1,k2,tk2,res);
+    antenatoAux(u->right,k1,tk1,k2,tk2,res);
+}
+
+
+bool antenato(Tree t, int k1, int k2){
+    bool tk1=false;
+    bool tk2=false;
+    bool res=true;
+
+    antenatoAux(t->root,k1,tk1,k2,tk2,res);
+
+    return res;
+}
+
+int main(){
+    Tree radice=new T;
+    radice->root = new Node{5, new Node{2, nullptr, nullptr}, new Node{6, nullptr, nullptr}};
+
+    cout<<"antenato? "<< antenato(radice,5,6);
+
+
+    return 0;
+}
