b991e76f344b13d9ae10e9c3fd38f763679caf94
JeremyFamengo <jeremydj3@gmail.com>
2024-06-11 10:39:02 +0200

    finito es1 pt2 rifatto 06-06-2022

---
+++ Esami completi/06-06-2022/es1_pt2RR.cpp
@@ -0,0 +1,73 @@
+#include "iostream"
+using namespace std;
+
+struct Node{
+    int key;
+    Node* left;
+    Node* right;
+    Node(int data) {
+        this->key = data;
+        left = right = NULL;
+    }
+};
+
+typedef Node* PNode;
+
+struct Tree{
+    PNode root;
+};
+
+typedef Tree* PTree;
+
+int print_aux(PNode n){ //T(n)=O(n) per il teorema della sostituzione
+    if(!n)return 0;
+    int sumsx= print_aux(n->left);
+    int sumdx= print_aux(n->right);
+    if(sumsx>sumdx){
+        cout<<n->key<<" ";
+    }
+    return sumsx+sumdx+1;
+}
+
+void print(PTree t){ //T(n)=O(n) per print_aux
+    print_aux(t->root);
+}
+
+void printBT(const std::string& prefix,  PNode node, bool isLeft)
+{
+    if( node != nullptr )
+    {
+        std::cout << prefix;
+
+        std::cout << (isLeft ? "├──" : "└──" );
+
+        // print the value of the node
+        std::cout << node->key << std::endl;
+
+        // enter the next tree level - left and right branch
+        printBT( prefix + (isLeft ? "│   " : "    "), node->left, true);
+        printBT( prefix + (isLeft ? "│   " : "    "), node->right, false);
+    }
+}
+
+void printBT(const PTree t)
+{
+    printBT("", t->root, false);
+}
+
+
+int main() {
+    PNode root = new Node(4);
+    root->left = new Node(2);
+    root->right = new Node(5);
+    root->left->left = new Node(1);
+    root->left->right = new Node(3);
+
+    PTree t = new Tree{root};
+
+    printBT(t);
+
+    print(t);
+
+    return 0;
+}
